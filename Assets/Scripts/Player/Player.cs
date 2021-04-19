b95fe36d61904576e8f4e2f682df02e7047682a1
Jarrett <jaysternumber1@gmail.com>
2021-04-19 13:18:02 -0600

    Movement Updated
    Movement is now solely based on the new input system

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -14,28 +14,67 @@ public class Player : MonoBehaviour
 
     [Header("Components")]
     public Rigidbody2D rb;
-    public Animator animator;
+    // public Animator animator;
+
+    [Header("Physics")]
+    public float maxSpeed = 7f;
+    public float linearDrag = 4f;
+
+
+  
     private void Update()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
+    
+        
 
     }
+    private void keysPressed()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            direction.x = 1;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            direction.x = -1;
+        }
+    }
+
     public void move(InputActionReference action)
     {
         Debug.Log("Move");
     }
+       
     private void FixedUpdate()
     {
+        keysPressed();
         moveCharacter(direction.x);
+        modifyPhysics();
     }
     void moveCharacter(float horizontal)
     {
         rb.AddForce(Vector2.right * horizontal * moveSpeed);
 
-        animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
-    }
+        if(Mathf.Abs(rb.velocity.x) > maxSpeed)
+        {
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
 
+        //animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
+    }
+   
+    void modifyPhysics()
+    {
+        bool changingDirection = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+        if(Mathf.Abs(direction.x) < 0.9 || changingDirection)
+        {
+            rb.drag = linearDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
        /* if((horizontal > 0 && !facingRight) || (horizontal< 0 && facingRight)){
         Flip();
 }
