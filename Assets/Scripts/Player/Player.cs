cd1f7a6d1305dc70b6513845712350d84158b89e
Jarrett <jaysternumber1@gmail.com>
2021-04-23 14:42:35 -0600

    Added Jump, Need to continue working on the Ground Check

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,21 +12,34 @@ public class Player : MonoBehaviour
     public Vector2 direction;
    
     public float sprintSpeedMultiplier = 1f;
-   // private bool facingRight = true;
+    // private bool facingRight = true;
+
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 15f;
   
     [Header("Components")]
     public Rigidbody2D rb;
     // public Animator animator;
+    public LayerMask groundLayer;
+
 
     [Header("Physics")]
     public float maxSpeed = 7f;
     public float linearDrag = 4f;
+    public float gravity = 1;
+    public float fallMultiplier = 5;
+
+
+    [Header("Collisions")]
+    public bool onGround = false;
+    public float groundLength = 0.6f;
 
 
   
     private void Update()
     {
-    
+        onGround = Physics.Raycast(transform.position, Vector2.down, groundLength, groundLayer);
+       
         
 
     }
@@ -46,6 +59,10 @@ public class Player : MonoBehaviour
         if (Keyboard.current.leftShiftKey.isPressed)
         {
             sprintSpeedMultiplier = 1.5f;
+        }
+        if (Keyboard.current.spaceKey.isPressed && onGround)
+        {
+            Jump();
         }
         
     }
@@ -74,28 +91,54 @@ public class Player : MonoBehaviour
 
         //animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
     }
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    }
    
     void modifyPhysics()
     {
         bool changingDirection = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
-        if(Mathf.Abs(direction.x) == 0 || changingDirection)
+        if (onGround)
         {
-            rb.drag = linearDrag;
+            if (Mathf.Abs(direction.x) == 0 || changingDirection)
+            {
+                rb.drag = linearDrag;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
         }
         else
         {
-            rb.drag = 0;
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+            if (rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * fallMultiplier;
+            } else if(rb.velocity.y > 0 && !Keyboard.current.spaceKey.isPressed)
+            {
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
         }
     }
-       /* if((horizontal > 0 && !facingRight) || (horizontal< 0 && facingRight)){
-        Flip();
+    /* if((horizontal > 0 && !facingRight) || (horizontal< 0 && facingRight)){
+     Flip();
 }
-    void Flip()
+ void Flip()
+ {
+     facingRight = !facingRight;
+     transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+ }
+
+ */
+
+    private void OnDrawGizmos()
     {
-        facingRight = !facingRight;
-        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);
     }
 
-    */
-    
 }
